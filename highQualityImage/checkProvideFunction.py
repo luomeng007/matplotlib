20687e7651b9ac9c8624269f475405ee8f71601e
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-11-23 14:07:07 +0100

    Add files via upload

---
+++ highQualityImage/checkProvideFunction.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+"""
+author: 15025
+age: 26
+e-mail: 1502506285@qq.com
+time: 2020/11/1912:21
+software: PyCharm
+
+Description:
+
+"""
+import matplotlib.pyplot
+
+# print all functions in a row
+print(dir(matplotlib.pyplot))
+# print all function in a column
+print("\n".join(dir(matplotlib.pyplot)))
