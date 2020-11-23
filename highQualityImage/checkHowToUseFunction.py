1188823fabbea9585ed74af4e327955c9100ede6
Luo Meng <53254812+luomeng007@users.noreply.github.com>
2020-11-23 14:09:30 +0100

    Add files via upload

---
+++ highQualityImage/checkHowToUseFunction.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+"""
+author: 15025
+age: 26
+e-mail: 1502506285@qq.com
+time: 2020/11/1912:21
+software: PyCharm
+
+Description:
+
+"""
+import matplotlib.pyplot as plt
+
+# check help documentation
+# this one is not usefully, we could press control and click on function to check its documentation
+print(help(plt.plot))
